d39b8d54d215df5693f32e000ba9d0db01ab967c
jasperayawan <ejayawan22@gmail.com>
2023-06-09 15:56:54 +0800

    update file uploader with multer

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser')
 const authRoute = require('./routes/auth')
 const userRoute = require('./routes/users')
 const postRoute = require('./routes/posts')
+const categoriesRoute = require('./routes/categories')
+const multer = require('multer')
 
 dotenv.config();
 const port = process.env.PORT;
@@ -15,9 +17,25 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
+const storage = multer.diskStorage({
+    destination:(req,file,callback) => {
+        callback(null, "images")
+    },filename:(req,file,callback) => {
+        callback(null, "hello.jpeg");
+    },
+})
+
+//upload file in image storage
+
+const upload = multer({storage: storage});
+app.post('/api/upload', upload.single('file'),(req, res) => {
+    res.status(200).json("File has been uploaded")
+})
+
 app.use('/api/auth', authRoute)
 app.use('/api/users', userRoute)
 app.use('/api/posts', postRoute)
+app.use('/api/categories', categoriesRoute)
 
 app.listen(port, () => {
     connect();
